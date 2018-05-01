4ca2a4cc7a1db172e10542aaa88d35944c9f1c85
stnDani <daniel.florentin.stoian@gmail.com>
2018-05-01 16:37:11 +0300

    added new files to cache

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
 var staticCacheName = 'cache-rest-rev-1';
 var filesCached = [
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/js/restaurant_info.js',
-    '/js/main.js',
-    '/service-worker.js'
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'royal-silver.jpg',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'js/restaurant_info.js',
+    'js/main.js',
+    'js/dbhelper.js'
 ];
 
 self.addEventListener('install', function(event) {
